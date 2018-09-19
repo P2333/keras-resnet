218300e3390e89339867bac3707d63ed9f98d0b7
Your Name <you@example.com>
2018-09-20 03:29:48 +0800

    add 3 models

---
+++ cifar10_resnet_EE_plus_3models.py
@@ -0,0 +1,296 @@
+"""Trains a ResNet on the CIFAR10 dataset.
+ResNet v1
+[a] Deep Residual Learning for Image Recognition
+https://arxiv.org/pdf/1512.03385.pdf
+ResNet v2
+[b] Identity Mappings in Deep Residual Networks
+https://arxiv.org/pdf/1603.05027.pdf
+"""
+
+from __future__ import print_function
+import keras
+from keras.layers import Dense, Conv2D, BatchNormalization, Activation
+from keras.layers import AveragePooling2D, Input, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l2
+from keras import backend as K
+from keras.models import Model
+from keras.datasets import cifar10
+import tensorflow as tf
+import numpy as np
+import os
+from model import resnet_v1, resnet_v2
+
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_float('lamda', 0.01, "lamda for Ensemble Entropy(EE)")
+tf.app.flags.DEFINE_float('nonME_lamda', 0.01, "lamda for non-ME")
+tf.app.flags.DEFINE_bool('augmentation', False, "whether use data augmentation")
+
+
+
+# Training parameters
+batch_size = 32  # orig paper trained all networks with batch_size=128
+epochs = 200
+num_classes = 10
+log_offset = 1e-20
+
+
+# Subtracting pixel mean improves accuracy
+subtract_pixel_mean = True
+
+# Model parameter
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
+n = 3
+
+# Model version
+# Orig paper: version = 1 (ResNet v1), Improved ResNet: version = 2 (ResNet v2)
+version = 1
+
+# Computed depth from supplied model parameter n
+if version == 1:
+    depth = n * 6 + 2
+elif version == 2:
+    depth = n * 9 + 2
+
+# Model name, depth and version
+model_type = 'ResNet%dv%d' % (depth, version)
+
+# Load the CIFAR10 data.
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Input image dimensions.
+input_shape = x_train.shape[1:]
+
+# Normalize data.
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# If subtract pixel mean is enabled
+if subtract_pixel_mean:
+    x_train_mean = np.mean(x_train, axis=0)
+    x_train -= x_train_mean
+    x_test -= x_train_mean
+
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+print('y_train shape:', y_train.shape)
+
+# Convert class vectors to binary class matrices.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+    # Arguments
+        epoch (int): The number of epochs
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+def Entropy(input):
+    #input shape is batch_size X num_class
+    return tf.reduce_sum(-tf.multiply(input, tf.log(input + log_offset)), axis=-1)
+
+def Ensemble_Entropy(y_true, y_pred, num_model=3, batch_size=batch_size):
+    y_p_1, y_p_2, y_p_3 = tf.split(y_pred, num_model, axis=-1)
+    y_t_1, y_t_2, y_t_3 = tf.split(y_true, num_model, axis=-1)
+    Ensemble = Entropy((y_p_1 + y_p_2 + y_p_3) / num_model)
+    return Ensemble
+
+def non_ME (y_true, y_pred, num_model=3, batch_size=batch_size):
+    ONE = tf.ones_like(y_true) # batch_size X (num_class X num_models)
+    R_y_true = ONE - y_true
+    non_y_pred = tf.multiply(R_y_true, y_pred)
+    y_p_1, y_p_2, y_p_3 = tf.split(non_y_pred, num_model, axis=-1)
+    non_me1 = Entropy(y_p_1 / tf.reduce_sum(y_p_1, axis=-1, keep_dims=True))
+    non_me2 = Entropy(y_p_2 / tf.reduce_sum(y_p_2, axis=-1, keep_dims=True))
+    non_me3 = Entropy(y_p_3 / tf.reduce_sum(y_p_3, axis=-1, keep_dims=True))
+    return non_me1, non_me2, non_me3
+
+def Ensemble_Entropy_metric(y_true, y_pred, num_model=3, batch_size=batch_size):
+    EE = Ensemble_Entropy(y_true, y_pred, num_model=num_model, batch_size=batch_size)
+    return K.mean(EE)
+
+def non_ME_metric (y_true, y_pred, num_model=3, batch_size=batch_size):
+    non_me1, non_me2, non_me3 = non_ME(y_true, y_pred, num_model, batch_size)
+    return (non_me1 + non_me2 + non_me3) / num_model
+
+def acc_metric(y_true, y_pred, num_model=3, batch_size=batch_size):
+    y_p_1, y_p_2, y_p_3 = tf.split(y_pred, num_model, axis=-1)
+    y_t_1, y_t_2, y_t_3 = tf.split(y_true, num_model, axis=-1)
+    acc1 = keras.metrics.categorical_accuracy(y_t_1, y_p_1)
+    acc2 = keras.metrics.categorical_accuracy(y_t_2, y_p_2)
+    acc3 = keras.metrics.categorical_accuracy(y_t_3, y_p_3)
+    return (acc1 + acc2 + acc3) / num_model
+
+def Loss_withEE_plus(y_true, y_pred, num_model=3):
+    y_p_1, y_p_2, y_p_3 = tf.split(y_pred, num_model, axis=-1)
+    y_t_1, y_t_2, y_t_3 = tf.split(y_true, num_model, axis=-1)
+    CE_1 = keras.losses.categorical_crossentropy(y_t_1, y_p_1)
+    CE_2 = keras.losses.categorical_crossentropy(y_t_2, y_p_2)
+    CE_3 = keras.losses.categorical_crossentropy(y_t_3, y_p_3)
+    EE = Ensemble_Entropy(y_true, y_pred, num_model)
+    non_me1, non_me2, non_me3 = non_ME(y_true, y_pred, num_model)
+    return CE_1 + CE_2 + CE_3 - FLAGS.lamda * EE + FLAGS.nonME_lamda * (non_me1 + non_me2 + non_me3)
+
+model_input = Input(shape=input_shape)
+
+if version == 2:
+    model_0, in_0, out_0 = resnet_v2(input=model_input, depth=depth)
+    model_1, in_1, out_1 = resnet_v2(input=model_input, depth=depth)
+    model_2, in_2, out_2 = resnet_v2(input=model_input, depth=depth)
+
+else:
+    model_0, in_0, out_0 = resnet_v1(input=model_input, depth=depth)
+    model_1, in_1, out_1 = resnet_v1(input=model_input, depth=depth)
+    model_2, in_2, out_2 = resnet_v1(input=model_input, depth=depth)
+
+model_output = keras.layers.concatenate([out_0, out_1, out_2])
+#model_output = keras.layers.Activation('linear')(model_output_merge)
+
+model = Model(input=model_input, output=model_output)
+
+if int(FLAGS.lamda) == 0:
+    print('No Ensemble_Entropy term')
+    model.compile(
+    loss='categorical_crossentropy',
+    optimizer=Adam(lr=lr_schedule(0)),
+    metrics=[acc_metric, Ensemble_Entropy_metric, non_ME_metric])
+else:
+    print('Have Ensemble_Entropy term')
+    model.compile(
+        loss=Loss_withEE_plus,
+        optimizer=Adam(lr=lr_schedule(0)),
+        metrics=[acc_metric, Ensemble_Entropy_metric, non_ME_metric])
+model.summary()
+print(model_type)
+
+# Prepare model model saving directory.
+save_dir = os.path.join(os.getcwd(), 'EEplus_3models_saved_models_lamda'+str(FLAGS.lamda)+'_nonMElamda'+str(FLAGS.nonME_lamda)+'_'+str(FLAGS.augmentation))
+model_name = 'cifar10_%s_model.{epoch:03d}.h5' % model_type
+if not os.path.isdir(save_dir):
+    os.makedirs(save_dir)
+filepath = os.path.join(save_dir, model_name)
+
+# Prepare callbacks for model saving and for learning rate adjustment.
+checkpoint = ModelCheckpoint(
+    filepath=filepath, monitor='val_acc_metric', mode='max', verbose=2, save_best_only=True)
+
+lr_scheduler = LearningRateScheduler(lr_schedule)
+
+lr_reducer = ReduceLROnPlateau(
+    factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-6)
+
+callbacks = [checkpoint, lr_reducer, lr_scheduler]
+
+
+# Augment labels
+y_train_2 = np.concatenate([y_train, y_train, y_train], axis=-1)
+y_test_2 = np.concatenate([y_test, y_test, y_test], axis=-1)
+
+
+
+# Run training, with or without data augmentation.
+if not FLAGS.augmentation:
+    print('Not using data augmentation.')
+    model.fit(
+        x_train, y_train_2,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=(x_test, y_test_2),
+        shuffle=True,
+        verbose=2,
+        callbacks=callbacks)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        # set input mean to 0 over the dataset
+        featurewise_center=False,
+        # set each sample mean to 0
+        samplewise_center=False,
+        # divide inputs by std of dataset
+        featurewise_std_normalization=False,
+        # divide each input by its std
+        samplewise_std_normalization=False,
+        # apply ZCA whitening
+        zca_whitening=False,
+        # epsilon for ZCA whitening
+        zca_epsilon=1e-06,
+        # randomly rotate images in the range (deg 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally
+        width_shift_range=0.1,
+        # randomly shift images vertically
+        height_shift_range=0.1,
+        # set range for random shear
+        shear_range=0.,
+        # set range for random zoom
+        zoom_range=0.,
+        # set range for random channel shifts
+        channel_shift_range=0.,
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        # value used for fill_mode = "constant"
+        cval=0.,
+        # randomly flip images
+        horizontal_flip=True,
+        # randomly flip images
+        vertical_flip=False,
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
+
+    # Compute quantities required for featurewise normalization
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+
+    # Fit the model on the batches generated by datagen.flow().
+    model.fit_generator(
+        datagen.flow(x_train, y_train_2, batch_size=batch_size),
+        validation_data=(x_test, y_test_2),
+        epochs=epochs,
+        verbose=2,
+        workers=4,
+        callbacks=callbacks)
+
+# Score trained model.
+#scores = model.evaluate([x_test, x_test], [y_test, y_test], verbose=1)
+#print('Test loss:', scores[0])
+#print('Test accuracy:', scores[1])
